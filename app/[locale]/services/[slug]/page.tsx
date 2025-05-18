52ec1fab9cdb5fcbf1fae5a4f2638c426204de42
zufichris <zufichris@gmail.com>
2025-05-18 19:32:12 +0400

    translated Page-Services

--- app/[locale]/services/[slug]/page.tsx
+++ app/[locale]/services/[slug]/page.tsx
@@ -1,100 +1,103 @@
-import { ServicesData, ServicesI } from "../_components/ServicesData"
-import HeroHeader from "@/inc/HeroHeader"
-import Image from "next/image"
-import Link from "next/link"
-import { CardStyleOne } from "@/inc/Containers"
-import { FaChevronCircleRight } from "react-icons/fa";
+import HeroHeader from "@/inc/HeroHeader";
+import Image from "next/image";
+import Link from "next/link";
+import { getTranslations } from "next-intl/server";
+import { notFound } from "next/navigation";
+import { getServiceData, ServiceFooterCard } from "@/components/services";
 
-export async function generateStaticParams() {
-    return ServicesData.map((guide: ServicesI) => ({
-        slug: guide.slug
-    }))
-}
 
-const getServiceDataItem = async ({
-    params,
+export async function generateMetadata({
+  params,
 }: {
-    params: Promise<{ slug: string }>
-}) => {
-    const {slug} = await params
-    const serviceitem = await ServicesData.find((service) => service.slug === slug);
+  params: Promise<{ slug: string }>;
+}) {
+  const { slug } = await params
+  const t = await getTranslations("SERVICES")
+  const services = getServiceData(t)
+  const serviceItem = services.find(s => s.slug === slug)
+  if (!serviceItem) {
+    return notFound()
+  }
 
-    return serviceitem;
+  return {
+    title: `Elchai | ${t("title")}`,
+    description: t("description"),
+  };
 }
 
-export async function generateMetadata({ params }: { params: Promise<{ slug: string }> }) {
-    const serviceitem = await getServiceDataItem({ params });
+export async function ServiceDetailsPage({
+  params,
+}: {
+  params: Promise<{ slug: string }>;
+}) {
+  const { slug } = await params
+  const t = await getTranslations("SERVICES")
+  const services = getServiceData(t)
+  const serviceItem = services.find(s => s.slug === slug)
 
-    if (!serviceitem) {
-        return {
-            title: "Service Not Found",
-            description: "The service you are looking for does not exist.",
-        };
-    }
+  return (
+    <>
+      <HeroHeader
+        video={serviceItem?.media.video}
+        className="max-w-[836px]"
+        title={serviceItem?.hero}
+      />
 
-    return {
-        title: `${serviceitem.title}`,
-        description: serviceitem.description,
-    };
-}
+      <div className="py-8 lg:py-16">
+        <div className="main-container">
+          <p className="text-center max-w-[782px] font-bold uppercase mx-auto">
+            {serviceItem?.intro}
+          </p>
+        </div>
+      </div>
 
-const ServiceInnerPage = async ({ params }: { params: Promise<{ slug: string }> }) => {
+      <div className="py-8 lg:py-16">
+        <div className="main-container">
+          <div>
+            <div>{serviceItem?.content}</div>
+          </div>
 
-    const serviceitem = await getServiceDataItem({ params });
-
-    return (
-        <>
-            <HeroHeader video={serviceitem?.video} className="max-w-[836px]" title={serviceitem?.herotext} />
-
-            <div className="py-8 lg:py-16">
-                <div className="main-container">
-                    <p className="text-center max-w-[782px] font-bold uppercase mx-auto">{serviceitem?.introduction}</p>
+          <div>
+            <h3 className="text-eblue uppercase font-bold text-2xl text-center mb-8">
+              Technology Stack
+            </h3>
+            <div className="flex justify-center flex-wrap gap-4 mb-16 ">
+              {serviceItem?.stack.map((stack, index) => (
+                <div
+                  key={index}
+                  className="flex items-center justify-center aspect-video rounded-3xl p-4 bg-white/5 w-[calc(50%-16px)] lg:w-[calc(20%-16px)]"
+                >
+                  {stack.media ? (
+                    <Image
+                      src={stack.media}
+                      alt=""
+                      width={200}
+                      height={200}
+                      className="aspect-video object-center object-contain w-full h-full"
+                    />
+                  ) : (
+                    <h2 className="uppercase font-bold">{stack.name}</h2>
+                  )}
                 </div>
+              ))}
             </div>
-
-            <div className="py-8 lg:py-16">
-                <div className="main-container">
-                    <div>
-                        <div>{serviceitem?.content}</div>
-                    </div>
-
-                    <div>
-                        <h3 className="text-eblue uppercase font-bold text-2xl text-center mb-8">Technology Stack</h3>
-                        <div className="flex justify-center flex-wrap gap-4 mb-16 ">
-                            {serviceitem?.technologyStack && serviceitem?.technologyStack.length > 0 && serviceitem?.technologyStack.map((stack, index) => 
-                                <div key={index} className="flex items-center justify-center aspect-video rounded-3xl p-4 bg-white/5 w-[calc(50%-16px)] lg:w-[calc(20%-16px)]">
-                                    {stack.media ? (
-                                        <Image src={stack.media} alt="" width={200} height={200} className="aspect-video object-center object-contain w-full h-full" />
-                                    ) : (
-                                        <h2 className="uppercase font-bold">{stack.name}</h2>
-                                    )}
-                                    
-                                </div>
-                            )}
-                        </div>
-                        <div className="text-center">
-                            <Link href={serviceitem?.action?.url || ''} className="btn btn-main">{serviceitem?.action?.name || ''}</Link>
-                        </div>
-                        
-                    </div>
-                </div>
+            <div className="text-center">
+              <Link
+                href={serviceItem?.link?.url || ""}
+                className="btn btn-main"
+              >
+                {serviceItem?.link?.name || ""}
+              </Link>
             </div>
+          </div>
+        </div>
+      </div>
 
-            <div className="py-8 lg:py-16">
-				<div className="main-container">
-					<CardStyleOne>
-						<div className="pt-7 pb-10">
-							<h2 className="main-title no-decoration mb-2 text-center">Ready to accelerate your <span>digital transformation?</span></h2>
-							<p className="max-w-[703px] text-center mx-auto mb-8">Contact Elchai’s experts today to discuss your project and unlock your business's potential.</p>
-							<div className="flex items-center justify-center gap-4">
-								<Link href={'/contact-us'} className="btn btn-main">Book Your Meeting Now <FaChevronCircleRight /></Link>
-							</div>	
-						</div>
-					</CardStyleOne>
-				</div>
-			</div>
-        </>
-    )
-}
-
-export default ServiceInnerPage
+      <div className="py-8 lg:py-16">
+        <div className="main-container">
+          <ServiceFooterCard t={t} />
+        </div>
+      </div>
+    </>
+  );
+};
