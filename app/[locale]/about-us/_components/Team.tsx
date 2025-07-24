da1a9a23816c004e0a95c5fa947676a479ddf36f
joel <joel@phoenixgroupuae.com>
2025-07-25 02:23:23 +0800

    Update Team

--- app/[locale]/about-us/_components/Team.tsx
+++ app/[locale]/about-us/_components/Team.tsx
@@ -2,12 +2,13 @@ import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs"
 import { FaLinkedin } from "react-icons/fa";
 import Image from "next/image";
 import { useTranslations } from "next-intl";
+import React from "react";
 
 interface LeaderShipData {
-    name: string;
-    position: string;
-    image: string;
-    linkedIn?: string;
+  name: string;
+  position: string;
+  image: string;
+  linkedIn?: string;
 }
 
 const LeaderShip = () => {
@@ -17,8 +18,15 @@ const LeaderShip = () => {
     {
       name: "Flavio Elia",
       position: t("leadership.CEO"),
-      image: "/images/IMG_8725_result.webp",
+      image: "/images/IMG_8864.webp",
       /* linkedIn: "https://www.linkedin.com/in/flavio-elia/" */
+      linkedIn: null
+    },
+    {
+      name: "Stefano Curzio",
+      position: t("leadership.CFO"),
+      image: "/images/Stefano-CFO.jpeg",
+      /* linkedIn: "https://www.linkedin.com/" */
       linkedIn: null
     },
     {
@@ -36,57 +44,77 @@ const LeaderShip = () => {
       linkedIn: null
     },
     {
+      name: "Peshwar",
+      position: t("leadership.CM"),
+      image: "/images/peshwar.webp",
+      /* linkedIn: "https://www.linkedin.com/" */
+      linkedIn: null
+    },
+    {
       name: "Diana",
-      position: t("leadership.CFO"),
+      position: t("leadership.BM"),
       image: "/images/diana.webp",
       /* linkedIn: "https://www.linkedin.com/" */
       linkedIn: null
     },
     {
-      name: "Peshwar",
-      position: t("leadership.CMO"),
-      image: "/images/peshwar.webp",
+      name: "Charles",
+      position: t("leadership.AIM"),
+      image: "/images/Charles-AIM.webp",
       /* linkedIn: "https://www.linkedin.com/" */
       linkedIn: null
     },
-    
+    {
+      name: "Khalayan",
+      position: t("leadership.AIM"),
+      image: "/images/Khalyan-AIM.webp",
+      /* linkedIn: "https://www.linkedin.com/" */
+      linkedIn: null
+    },
+
   ];
 
   return (
-    <div className="grid grid-cols-1 lg:grid-cols-5 gap-8">
-      {LeaderShipData.map((item, index) => (
-        <div
-          key={index}
-          className="flex flex-col items-center gap-3 p-2 bg-linear-to-r from-transparent to-transparent hover:from-eblue hover:to-epurple transition-all hover:transition-all rounded-xl"
-        >
-          <div className="aspect-square rounded-xl overflow-hidden">
-            <Image
-              src={item.image}
-              alt={item.name}
-              width={768}
-              height={768}
-              className="object-cover object-center w-full h-full"
-            />
-          </div>
-          <div className="flex items-start justify-between gap-3 w-full">
-            <div>
-              <h3 className="font-bold">{item.name}</h3>
-              <p>{item.position}</p>
-            </div>
+    <div className="grid grid-cols-1 lg:grid-cols-5 gap-8 items-center">
+      {LeaderShipData.map((item, index) => {
 
-            {item.linkedIn && (
-              <a
-                href={item.linkedIn}
-                target="_blank"
-                rel="noopener noreferrer"
-                className="text-2xl"
-              >
-                <FaLinkedin />
-              </a>
-            )}
+        return (
+          <React.Fragment key={index}>
+          {index === 5 && <div></div>}
+          <div
+            key={index}
+            className="flex flex-col items-center gap-3 p-2 bg-linear-to-r from-transparent to-transparent hover:from-eblue hover:to-epurple transition-all hover:transition-all rounded-xl"
+          >
+            <div className="aspect-square rounded-xl overflow-hidden">
+              <Image
+                src={item.image}
+                alt={item.name}
+                width={768}
+                height={768}
+                className="object-cover object-center w-full h-full max-w-[300px]"
+              />
+            </div>
+            <div className="flex items-start justify-between gap-3 w-full max-w-[300px]">
+              <div>
+                <h3 className="font-bold">{item.name}</h3>
+                <p>{item.position}</p>
+              </div>
+
+              {item.linkedIn && (
+                <a
+                  href={item.linkedIn}
+                  target="_blank"
+                  rel="noopener noreferrer"
+                  className="text-2xl"
+                >
+                  <FaLinkedin />
+                </a>
+              )}
+            </div>
           </div>
-        </div>
-      ))}
+          </React.Fragment>
+        );
+      })}
     </div>
   );
 };
